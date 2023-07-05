e9a62598b25a48b37230850a3f0f5cc4930ce778
Luchy-B <okparabon@gmail.com>
2023-07-05 16:51:40 +0100

    Add styling

---
+++ db/migrate/20230705090707_create_foods.rb
@@ -0,0 +1,11 @@
+class CreateFoods < ActiveRecord::Migration[7.0]
+  def change
+    create_table :foods do |t|
+      t.string :name
+      t.string :unit
+      t.string :pric
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_05_083041) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_05_090707) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "unit"
+    t.string "pric"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
