3041a01123936f9f9f1486b646e0776be5d3c378
Luchy-B <okparabon@gmail.com>
2023-07-05 15:10:19 +0100

    Add controllers and views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,15 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    redirect_to recipes_path, notice: "#{recipe.name} deleted!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
+  root 'recipes#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :recipes
 end
