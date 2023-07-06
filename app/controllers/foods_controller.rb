dde74172307c5327cd12a737e7f727f209e0a4ed
Tracy-miranja <barazatracy44@gmail.com>
2023-07-06 10:55:07 +0300

    Modify authorisation method

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,27 +1,24 @@
 class FoodsController < ApplicationController
   before_action :set_food, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, expect: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
-  # GET /foods or /foods.json
   def index
     @foods = Food.all
   end
 
-  # GET /foods/1 or /foods/1.json
   def show
   end
 
-  # GET /foods/new
   def new
-    @food = Food.new
+    @food =current_user.foods.build
   end
 
-  # GET /foods/1/edit
   def edit
   end
 
-  # POST /foods or /foods.json
   def create
-    @food = Food.new(food_params)
+    @food =current_user.foods.build(food_params)
 
     respond_to do |format|
       if @food.save
@@ -34,7 +31,6 @@ class FoodsController < ApplicationController
     end
   end
 
-  # DELETE /foods/1 or /foods/1.json
   def destroy
     @food.destroy
 
@@ -44,13 +40,16 @@ class FoodsController < ApplicationController
     end
   end
 
+  def correct_user
+    @food = current_user.foods.find_by(id: params[:id])
+    redirect_to foods_path, notice: "Not authorized to edit." if @food.nil?
+  end  
+
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_food
       @food = Food.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def food_params
       params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
     end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,3 @@
 class Food < ApplicationRecord
-    
+    belongs_to:user
 end
