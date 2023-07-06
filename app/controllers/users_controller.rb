b2eee19ffdef6a76ae82e9eb9f31f3b75cbc0b4f
Tracy-miranja <barazatracy44@gmail.com>
2023-07-06 22:40:17 +0300

    Modify shopping list page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+    def index
+      # Your code here
+    end
+  
+    def show
+        
+      end
+  
+    def new
+      # Your code here
+    end
+  
+    def create
+      # Your code here
+    end
+  
+    # Add more actions as needed
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   resources :foods, only: [:create, :index, :destroy, :new]
-  resources :users do
-    resource :shopping_list, only: [:show], controller: 'shopping_lists'
-  end
+  get 'general_shopping_list', to: 'general_shopping_list#index'
+  resources :recipes 
+ 
   
-  resources :recipes
   
+
   devise_for :users
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
   root "foods#index"
 end
 
